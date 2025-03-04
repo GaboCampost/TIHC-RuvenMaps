6031883c480c98f661262b4c26a20078860fce0a
Gabriel <65680061+GaboCampost@users.noreply.github.com>
2025-03-04 20:06:03 +0000

    Create code.js

---
+++ js/code.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Selecciona los botones y el sidebar
+    const dashboardBtn = document.querySelector('.sidebar-btn');
+    const sidebar = document.getElementById('sidebar');
+
+    // Función para alternar las clases del sidebar
+    function toggleSidebar() {
+        sidebar.classList.toggle('sidebar-hidden');
+        sidebar.classList.toggle('sidebar-visible');
+        dashboardBtn.classList.toggle('sidebar-hidden');
+        dashboardBtn.classList.toggle('sidebar-visible');
+    }
+
+    // Añade el evento click a los botones
+    if (dashboardBtn && sidebar) {
+        dashboardBtn.addEventListener('click', toggleSidebar);
+    } else {
+        console.error('No se encontraron uno o más elementos en el DOM.');
+    }
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const searchInput = document.getElementById('search-input');
+    const suggestionBox = document.querySelector('.suggestion-box');
+
+    // Mostrar la caja de sugerencias cuando el input está en foco
+    searchInput.addEventListener('focus', function() {
+        suggestionBox.style.display = 'block';
+    });
+
+    // Ocultar la caja de sugerencias cuando el input pierde el foco
+    searchInput.addEventListener('blur', function() {
+        suggestionBox.style.display = 'none';
+    });
+});
